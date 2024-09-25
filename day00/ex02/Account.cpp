cacc13087ee818e05ea313efda0cbb5aa3355de5
soufiane <soufianeessarhir@gmail.com>
2024-09-25 19:59:19 +0000

    in progress

--- day00/ex02/Account.cpp
+++ day00/ex02/Account.cpp
@@ -12,26 +12,68 @@
 
 #include "Account.hpp"
 #include <iostream>
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <time.h>
+#include<iomanip>
 
+
+std::string getCurrentTimestamp() 
+{
+    std::stringstream ss;
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    ss << std::put_time(std::localtime(&now), "[%Y%m%d_%H%M%S]");
+    return ss.str();
+}
 int Account::getNbAccounts(){return  _nbAccounts;}
 int Account::getTotalAmount(){return _totalAmount;}
 int Account::getNbDeposits(){return _totalNbDeposits;}
 int Account::getNbWithdrawals(){return _totalNbWithdrawals;}
 void Account::displayAccountsInfos() 
-{}
+{
+    std::cout<<getCurrentTimestamp()<<" accounts:"
+    <<_nbAccounts<<";total:"<<_totalAmount<<";deposits:"
+    <<_totalNbDeposits<<":withdrawals:"
+    <<_totalNbWithdrawals<<std::endl;
+}
 Account::Account(int initial_deposit)
 {
-    
+    _amount = initial_deposit;
+    _accountIndex = _nbAccounts;
+    _nbAccounts++;
+    _totalAmount+=initial_deposit;
+    _nbWithdrawals=0;
+    _nbDeposits=0;
+    std::cout<<getCurrentTimestamp()<<" index:"
+    <<_accountIndex<<";amount:"<<_amount<<"created\n";
+
 }
-Account::~Account()
+void Account::makeDeposit(int deposit)
+{
+    _nbDeposits++;
+    _totalNbDeposits++;
+
+}
+
+
+
+bool	Account::makeWithdrawal( int withdrawal ){return _amount >= withdrawal ? false : true;}
+
+int		Account::checkAmount( void )const {return _amount;}
+
+void	Account::displayStatus( void ) const{}
+
+void	Account::_displayTimestamp( void )
 {
 
 }
-void Account::makeDeposit(int deposit){}
 
-bool	Account::makeWithdrawal( int withdrawal ){}
 
-int		Account::checkAmount( void )const {}
-void	Account::displayStatus( void ) const{}
-void	Account::_displayTimestamp( void ){}
-Account::Account( void ){}
+
+
+
+
+
+Account::~Account() {std::cout<<"destructor is called\n";}
+Account::Account( void ){ std::cout<<"default constructor is called\n";} 
