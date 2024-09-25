d5d96bea3a2a0c3a4d15f42148abd1a80dfb4311
soufiane <soufianeessarhir@gmail.com>
2024-09-25 16:12:44 +0000

    starting

--- day00/ex02/Account.cpp
+++ day00/ex02/Account.cpp
@@ -11,4 +11,27 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
+int Account::getNbAccounts(){return  _nbAccounts;}
+int Account::getTotalAmount(){return _totalAmount;}
+int Account::getNbDeposits(){return _totalNbDeposits;}
+int Account::getNbWithdrawals(){return _totalNbWithdrawals;}
+void Account::displayAccountsInfos() 
+{}
+Account::Account(int initial_deposit)
+{
+    
+}
+Account::~Account()
+{
+
+}
+void Account::makeDeposit(int deposit){}
+
+bool	Account::makeWithdrawal( int withdrawal ){}
+
+int		Account::checkAmount( void )const {}
+void	Account::displayStatus( void ) const{}
+void	Account::_displayTimestamp( void ){}
+Account::Account( void ){}
