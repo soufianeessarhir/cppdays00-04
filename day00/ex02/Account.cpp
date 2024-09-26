dcf8051ebb9f53e40eee03d9a5e702eba0c3600f
soufiane <soufianeessarhir@gmail.com>
2024-09-26 07:48:24 +0000

    in fixing

--- day00/ex02/Account.cpp
+++ day00/ex02/Account.cpp
@@ -18,21 +18,14 @@
 #include <time.h>
 #include<iomanip>
 
-
-std::string getCurrentTimestamp() 
-{
-    std::stringstream ss;
-    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    ss << std::put_time(std::localtime(&now), "[%Y%m%d_%H%M%S]");
-    return ss.str();
-}
 int Account::getNbAccounts(){return  _nbAccounts;}
 int Account::getTotalAmount(){return _totalAmount;}
 int Account::getNbDeposits(){return _totalNbDeposits;}
 int Account::getNbWithdrawals(){return _totalNbWithdrawals;}
 void Account::displayAccountsInfos() 
 {
-    std::cout<<getCurrentTimestamp()<<" accounts:"
+    _displayTimestamp();
+    std::cout<<"accounts:"
     <<_nbAccounts<<";total:"<<_totalAmount<<";deposits:"
     <<_totalNbDeposits<<":withdrawals:"
     <<_totalNbWithdrawals<<std::endl;
@@ -45,7 +38,8 @@ Account::Account(int initial_deposit)
     _totalAmount+=initial_deposit;
     _nbWithdrawals=0;
     _nbDeposits=0;
-    std::cout<<getCurrentTimestamp()<<" index:"
+    _displayTimestamp();
+    std::cout<<"index:"
     <<_accountIndex<<";amount:"<<_amount<<"created\n";
 
 }
@@ -53,27 +47,55 @@ void Account::makeDeposit(int deposit)
 {
     _nbDeposits++;
     _totalNbDeposits++;
-
+    _displayTimestamp();
+    std::cout
+    <<"index:"<<_accountIndex<<";p_amount:"<<_amount<<";deposit:"<<deposit
+    <<";amount:"<<_amount+deposit<<";nb_deposits:"<<_nbDeposits<<std::endl;
+    _amount+=deposit;
+    _totalAmount+=deposit;
 }
 
 
 
-bool	Account::makeWithdrawal( int withdrawal ){return _amount >= withdrawal ? false : true;}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+   if (_amount >= withdrawal)
+   {
+    _nbWithdrawals++;
+    _totalNbWithdrawals++;
+    _displayTimestamp();
+     std::cout
+     <<"index:"<<_accountIndex<<";p_amount:"<<_amount<<";withdrawal:"<<withdrawal
+     <<";amount:"<<_amount-withdrawal<<";nb_withdrawals:"<<_nbWithdrawals<<std::endl;
+     _amount-=withdrawal;
+     _totalAmount-=withdrawal;
+     return true;
+    
+   }
+    _displayTimestamp();
+    std::cout
+    <<"index:"<<_accountIndex<<";p_amount:"<<_amount<<";withdrawal:"<<"refused\n";
+    return false;
+}
 
-int		Account::checkAmount( void )const {return _amount;}
 
-void	Account::displayStatus( void ) const{}
+void	Account::displayStatus( void ) const
+{
+    _displayTimestamp();
+    std::cout<<" "
+    <<"accounts:"<<_nbAccounts<<";total:"<<_totalAmount<<";deposits:"
+    <<_totalNbDeposits<<";withdrawals:"<<_totalNbWithdrawals<<std::endl;
+
+}
 
 void	Account::_displayTimestamp( void )
 {
-
+    std::stringstream ss;
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    ss << std::put_time(std::localtime(&now), "[%Y%m%d_%H%M%S]");
+    std::cout<<ss.str()<<" ";
 }
 
-
-
-
-
-
-
+int		Account::checkAmount( void )const {return _amount;}
 Account::~Account() {std::cout<<"destructor is called\n";}
 Account::Account( void ){ std::cout<<"default constructor is called\n";} 
