66f02ad90df8c54949b0d8864e09fe04dbdd3bc3
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-10-11 14:28:05 +0100

    fixing

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -12,20 +12,18 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
 int main(int ac,char **av)
 {
-    if (ac > 1)
+    if (ac == 1)
+       return(std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl, 0); 
+    for(int i = 1; i < ac; i++)
     {
-        for(size_t i = 1; i < (size_t)ac; i++)
-        {
-            std :: string str = av[i];
-            for(size_t j = 0; j < str.length(); j++)
-                std :: cout << (char)std :: toupper(str[j]);
-        }
-        std :: cout << std :: endl;
+        std :: string str = av[i];
+        for(std::string::iterator it = str.begin(); it != str.end(); ++it)
+            *it = static_cast<char>(std::toupper(static_cast<unsigned char>(*it)));
+        std :: cout << str;
     }
-    else
-        std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl;
-    return 0;
+    return (std :: cout << std :: endl,0);
 }
